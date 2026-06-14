57f94fc7f3c1a0e2d96785bc81f71e215e4ac2dd
PT KAYVARA INDONESIA <reyhansyahputra2003@gmail.com>
2026-06-14 18:20:21 +0800

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,25 @@ const chatButton = document.getElementById("chatButton");
 chatButton.addEventListener("click", function () {
     window.open("https://direct.lc.chat/19779830", "_blank");
 });
+const chatButton = document.getElementById("chatButton");
+
+chatButton.addEventListener("click", function () {
+    window.open("https://direct.lc.chat/19779830", "_blank");
+});
+
+
+// NAVBAR FUNCTION
+const navItems = document.querySelectorAll(".nav-item");
+
+navItems.forEach(item => {
+    item.addEventListener("click", function () {
+
+        navItems.forEach(i => i.classList.remove("active"));
+        this.classList.add("active");
+
+        if (this.textContent.includes("Chat")) {
+            window.open("https://direct.lc.chat/19779830", "_blank");
+        }
+
+    });
+});
