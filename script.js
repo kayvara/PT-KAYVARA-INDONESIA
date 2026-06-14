a4d4d22f45d753d518ea40e42a901e78ec2abe32
PT KAYVARA INDONESIA <reyhansyahputra2003@gmail.com>
2026-06-14 18:18:04 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
 const chatButton = document.getElementById("chatButton");
 
 chatButton.addEventListener("click", function () {
-
-    alert("Menghubungkan ke Customer Care...");
-
-    // Ganti link di bawah dengan link Live Chat kamu
-    window.location.href = "https://contoh-link-livechat.com";
-
+    window.location.href = "https://direct.lc.chat/19779830";
 });
